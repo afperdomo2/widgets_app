0adf772d8315a221922ce79472449adb26abb20b
alvarado0925 <alvarado0925@gmail.com>
2025-02-02 13:36:34 -0500

    Agregar vista de tarjetas en CardsScreen con diferentes niveles de elevación

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>>[
+  {"elevation": 0.0, "title": "Elevation 0"},
+  {"elevation": 1.0, "title": "Elevation 1"},
+  {"elevation": 2.0, "title": "Elevation 2"},
+  {"elevation": 3.0, "title": "Elevation 3"},
+  {"elevation": 4.0, "title": "Elevation 4"},
+  {"elevation": 5.0, "title": "Elevation 5"},
+];
+
 class CardsScreen extends StatelessWidget {
   static const String name = 'cards_screen';
 
@@ -11,7 +20,48 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Cards"),
       ),
-      body: const Placeholder(),
+      body: const _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ...cards.map((card) => _CardType1(card: card)),
+      ],
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  const _CardType1({required this.card});
+
+  final Map<String, dynamic> card;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: card['elevation'],
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert_outlined)),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(card['title']),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
