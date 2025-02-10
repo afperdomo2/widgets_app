204b6d9813df0f83e18885f38bb0bc508976400a
alvarado0925 <alvarado0925@gmail.com>
2025-02-09 23:19:03 -0500

    Agrega ícono de modo oscuro en counter_screen y crea el proveedor isDarkMode

--- lib/presentation/screens/counter/counter_screen.dart
+++ lib/presentation/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/providers/counter.riverpod.dart';
+import 'package:widgets_app/providers/theme.riverpod.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const String name = 'counter_screen';
@@ -10,16 +11,25 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int clickCounter = ref.watch(counterProvider);
+    final bool isDarkMode = ref.watch(isDarkModeProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Riverpod Counter"),
-      ),
+      appBar: AppBar(title: Text("Riverpod Counter $isDarkMode"), actions: [
+        IconButton(
+          icon: Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode),
+          onPressed: () {
+            ref.read(isDarkModeProvider.notifier).state = !isDarkMode;
+          },
+        )
+      ]),
       body: Center(
         child: Text("Valor: $clickCounter", style: Theme.of(context).textTheme.titleLarge),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).state++;
+          // ref.read(counterProvider.notifier).update((value) => value + 1); // Alternativa para incrementar el valor
+        },
         child: const Icon(Icons.add),
       ),
     );
